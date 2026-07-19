97937eea2d22b06099c4c16ee5d3d780454a04fa
Lin mb <245150191+xf280230439-netizen@users.noreply.github.com>
2026-07-19 09:57:36 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'linzhonggui-shell-10ff831871'
+const PRECACHE = ["./","./assets/sqlite3-BVKGSWc-.wasm","./assets/sqlite3-opfs-async-proxy-D_xnb1D8.js","./assets/sqlite3-worker1-DfZCUurM.js","./index.html","./manifest.webmanifest"]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request
+  if (request.method !== 'GET' || new URL(request.url).origin !== self.location.origin) return
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy))
+          return response
+        })
+        .catch(() => caches.match(request).then((cached) => cached || caches.match('./index.html'))),
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+      if (response.ok) caches.open(CACHE_NAME).then((cache) => cache.put(request, response.clone()))
+      return response
+    })),
+  )
+})
